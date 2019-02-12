cfe19efe2aa898f635e24cc31a25a351e5e65f48
Alexey Abrosimov <amberlex78@gmail.com>
2019-02-12 15:31:53 +0200

    updated: docker-compose up to 1.23.2

--- docker-install-mint18.sh
+++ docker-install-mint18.sh
@@ -67,7 +67,7 @@ if [ -a /usr/local/bin/docker-compose ]
     else
         # Releases: https://github.com/docker/compose/releases
         sudo curl \
-            -L https://github.com/docker/compose/releases/download/1.21.2/docker-compose-$(uname -s)-$(uname -m) \
+            -L https://github.com/docker/compose/releases/download/1.23.2/docker-compose-$(uname -s)-$(uname -m) \
             -o /usr/local/bin/docker-compose
         sudo chmod +x /usr/local/bin/docker-compose
 fi
