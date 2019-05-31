197dec12aa7bb4fd8f347030d5152c643592b5b0
Alexey Abrosimov <amberlex78@gmail.com>
2019-05-31 22:51:12 +0300

    Update docker-install-mint19.sh

--- docker-install-mint19.sh
+++ docker-install-mint19.sh
@@ -71,7 +71,7 @@ if [ -a /usr/local/bin/docker-compose ]
     else
         # Releases: https://github.com/docker/compose/releases
         sudo curl \
-            -L https://github.com/docker/compose/releases/download/1.23.2/docker-compose-$(uname -s)-$(uname -m) \
+            -L https://github.com/docker/compose/releases/download/1.24.0/docker-compose-$(uname -s)-$(uname -m) \
             -o /usr/local/bin/docker-compose
         sudo chmod +x /usr/local/bin/docker-compose
 fi
