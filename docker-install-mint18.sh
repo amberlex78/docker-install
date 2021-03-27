cf3127c655660042376744d93e587ce670f727dc
Alexey Abrosimov <amberlex78@gmail.com>
2021-03-27 05:14:34 +0200

    changed docker-compose 1.26.0 => 1.28.6

--- docker-install-mint18.sh
+++ docker-install-mint18.sh
@@ -73,7 +73,7 @@ if [ -a /usr/local/bin/docker-compose ]
     else
         # Releases: https://github.com/docker/compose/releases
         sudo curl \
-            -L https://github.com/docker/compose/releases/download/1.26.0/docker-compose-$(uname -s)-$(uname -m) \
+            -L https://github.com/docker/compose/releases/download/1.28.6/docker-compose-$(uname -s)-$(uname -m) \
             -o /usr/local/bin/docker-compose
         sudo chmod +x /usr/local/bin/docker-compose
 fi
