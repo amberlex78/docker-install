cfdd3d672e6b41dae8b83d3dd365ae97e3055160
amberlex78 <amberlex78@gmail.com>
2022-01-13 11:14:57 +0200

    fix and update docker-compose to v1.29.2

--- docker-install-mint20.sh
+++ docker-install-mint20.sh
@@ -73,7 +73,7 @@ if [ -a /usr/local/bin/docker-compose ]
     else
         # Releases: https://github.com/docker/compose/releases
         sudo curl \
-            -L https://github.com/docker/compose/releases/download/1.29.1/docker-compose-$(uname -s)-$(uname -m) \
+            -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" \
             -o /usr/local/bin/docker-compose
         sudo chmod +x /usr/local/bin/docker-compose
 fi
