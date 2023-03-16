d8afa8fde93e8109eb7ff4c8913a8430127f0d04
amberlex78 <amberlex78@gmail.com>
2023-03-16 22:09:32 +0200

    upgrade, add for linux Mint 21, rm 18

--- README.md
+++ README.md
@@ -9,28 +9,10 @@ git clone git@github.com:amberlex78/docker-install.git
 cd docker-install
 ~~~
 
-## Install Docker on Linux Mint 20
+### Install Docker on Linux Mint
 
-- [Linux Mint 20.3 "Una" - Cinnamon (64-bit)](https://linuxmint.com/edition.php?id=292)
-- [Linux Mint 20.3 "Una" - MATE (64-bit)](https://linuxmint.com/edition.php?id=293)
-
-For run script:
+`./docker-install-mint21.sh`
 
 `./docker-install-mint20.sh`
-## Install Docker on Linux Mint 19
-
-- [Linux Mint 19.3 "Tricia" - Cinnamon (64-bit)](https://linuxmint.com/edition.php?id=274)
-- [Linux Mint 19.3 "Tricia" - MATE (64-bit)](https://linuxmint.com/edition.php?id=276)
-
-For run script:
 
 `./docker-install-mint19.sh`
-
-## Install Docker on Linux Mint 18
-
-- [Linux Mint 18.3 "Sylvia" - Cinnamon (64-bit)](https://www.linuxmint.com/edition.php?id=246)
-- [Linux Mint 18.3 "Sylvia" - MATE (64-bit)](https://www.linuxmint.com/edition.php?id=248)
-
-For run script:
-
-`./docker-install-mint18.sh`
---
+++ docker-install-mint21.sh
@@ -0,0 +1,102 @@
+#!/bin/bash
+
+#############################################################################
+# Install Docker on Linux Mint 21
+#
+# https://github.com/amberlex78/docker-install
+#############################################################################
+
+echo ;
+echo "01------------ Update the APT packages indexes"
+sudo apt update
+
+
+echo ;
+echo "02------------ Uninstall old versions"
+sudo sudo systemctl stop docker.socket
+sudo apt purge -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin docker-ce-rootless-extras
+sudo apt update
+
+
+echo ;
+echo "03------------ Check packages"
+packages="ca-certificates curl gnupg lsb-release"
+for package in $packages
+do
+    cmd=$(dpkg -s $package 2>/dev/null | grep "ok installed")
+    if [ $? == 0 ]
+        then
+            echo "$package INSTALLED."
+        else
+            echo "$package NOT INSTALLED. Installing..."
+            sudo apt install -y $package
+    fi
+done
+
+
+echo ;
+echo "04------------ Add docker GPG key"
+sudo mkdir -m 0755 -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+
+
+echo ;
+echo "05------------ Add docker deb for Ubuntu Jammy 22.04 (Mint 21.x)"
+if [ -a /etc/apt/sources.list.d/docker.list ]
+    then
+        echo "The deb package is already exists."
+    else
+        # See PACKAGE BASE in https://linuxmint.com/download_all.php
+        echo "deb [arch=amd64 signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu jammy stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+fi
+
+
+echo ;
+echo "06------------ Install Docker"
+cmd=$(dpkg -s docker-ce 2>/dev/null | grep "ok installed")
+if [ $? == 0 ]
+    then
+        echo "The 'docker-ce' is already exists."
+    else
+        echo "The 'docker-ce' NOT INSTALLED. Installing..."
+        sudo apt update && sudo apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+fi
+
+
+echo ;
+echo "07------------ Add group 'docker'"
+group=$(getent group docker | grep "docker")
+if [ $? == 0 ]
+    then
+        echo "The group 'docker' is already exists."
+    else
+        sudo groupadd docker
+        echo "The group 'docker' was added."
+fi
+
+
+echo ;
+echo "08------------ Add user '$USER' to the 'docker' group"
+user_group=$(groups $USER | grep "docker")
+if [ $? == 0 ]
+    then
+        echo "The user '$USER' is already in 'docker' group."
+    else
+        sudo usermod -aG docker $USER
+        echo "User '$USER' was added to 'docker' group."
+fi
+
+
+echo ;
+echo "09------------ Check versions"
+docker -v
+docker compose version
+
+
+echo ;
+echo "10------------ docker run hello-word"
+docker run hello-world
+
+
+echo ;
+exit 0
